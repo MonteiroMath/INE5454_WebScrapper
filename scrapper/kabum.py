0dea73ee0c0197357bb763facb148aec4dbd6c76
Matheus Monteiro <math.a.monteiro@gmail.com>
2025-04-25 16:27:13 -0300

    Extracted extractData and navigateToNextPage functions

--- scrapper/kabum.py
+++ scrapper/kabum.py
@@ -6,6 +6,34 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 import time
 
+
+def extractData():
+    productCards = driver.find_elements(By.CLASS_NAME, "productCard")
+
+    pageData = []
+    url = driver.current_url
+    for productCard in productCards:
+        name = productCard.find_element(By.CLASS_NAME, "nameCard").text
+        oldPrice = productCard.find_element(By.CLASS_NAME, "oldPriceCard").text
+        currentPrice = productCard.find_element(
+            By.CLASS_NAME, "priceCard").text
+
+        productData = {"name": name, "active_sale": True if oldPrice else False, "old_price": oldPrice if oldPrice else None,
+                       "price": currentPrice, "url": url, "date": datetime.today().strftime('%Y-%m-%d')}
+
+        pageData.append(productData)
+
+    return pageData
+
+
+def navigateToNextPage():
+    nextButton = driver.find_element(By.CLASS_NAME, "nextLink")
+    driver.execute_script(
+        "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", nextButton)
+    time.sleep(0.5)
+    nextButton.click()
+
+
 options = Options()
 # options.add_argument("--headless")
 options.add_argument("--window-size=1920,1080")
@@ -16,30 +44,22 @@ driver.get("https://www.kabum.com.br/hardware")
 
 title = driver.title
 
-productCards = driver.find_elements(By.CLASS_NAME, "productCard")
-
 consentButton = driver.find_element(By.ID, "onetrust-accept-btn-handler")
 consentButton.click()
-
 data = []
-url = driver.current_url
-for productCard in productCards:
-    name = productCard.find_element(By.CLASS_NAME, "nameCard").text
-    oldPrice = productCard.find_element(By.CLASS_NAME, "oldPriceCard").text
-    currentPrice = productCard.find_element(By.CLASS_NAME, "priceCard").text
-
-    productData = {"name": name, "active_sale": True if oldPrice else False, "old_price": oldPrice if oldPrice else None,
-                   "price": currentPrice, "url": url, "date": datetime.today().strftime('%Y-%m-%d')}
-
-    data.append(productData)
+pageData = extractData()
+data.extend(pageData)
+print(data)
 
 
 wait = WebDriverWait(driver, timeout=100)
-nextButton = driver.find_element(By.CLASS_NAME, "nextLink")
-driver.execute_script(
-    "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", nextButton)
-time.sleep(0.5)
-nextButton.click()
+
+navigateToNextPage()
+
+WebDriverWait(driver, 30).until(EC.url_changes(driver.current_url))
+WebDriverWait(driver, 30).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "productCard"))
+)
 
 
 driver.quit()
