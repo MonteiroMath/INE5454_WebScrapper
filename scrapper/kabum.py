8643f9dff64ec84e6ee57eb44130ae0b6c4f0339
Matheus Monteiro <math.a.monteiro@gmail.com>
2025-04-25 16:16:02 -0300

    Navigation to the next page

--- scrapper/kabum.py
+++ scrapper/kabum.py
@@ -1,14 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
+import time
 
-driver = webdriver.Chrome()
+options = Options()
+# options.add_argument("--headless")
+options.add_argument("--window-size=1920,1080")
+driver = webdriver.Chrome(options=options)
+
 
 driver.get("https://www.kabum.com.br/hardware")
 
 title = driver.title
 
 productCards = driver.find_elements(By.CLASS_NAME, "productCard")
+
+consentButton = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+consentButton.click()
 
 data = []
 url = driver.current_url
@@ -22,6 +33,13 @@ for productCard in productCards:
 
     data.append(productData)
 
-print(data)
+
+wait = WebDriverWait(driver, timeout=100)
+nextButton = driver.find_element(By.CLASS_NAME, "nextLink")
+driver.execute_script(
+    "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", nextButton)
+time.sleep(0.5)
+nextButton.click()
+
 
 driver.quit()
