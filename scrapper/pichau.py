5510d371e998e75c4c4d198e0aa9a52dba57cd84
Matheus Monteiro <math.a.monteiro@gmail.com>
2025-05-05 16:28:27 -0300

    Pichau scrapper

--- scrapper/pichau.py
+++ scrapper/pichau.py
@@ -9,18 +9,35 @@ import json
 
 
 def extractData():
-    productCards = driver.find_elements(By.CLASS_NAME, "productCard")
+    productCards = driver.find_elements(
+        By.CSS_SELECTOR, "[class*='product_item']")
 
     pageData = []
     url = driver.current_url
     print(f"Extracting from {url}")
     for productCard in productCards:
-        name = productCard.find_element(By.CLASS_NAME, "nameCard").text
-        oldPrice = productCard.find_element(By.CLASS_NAME, "oldPriceCard").text
-        currentPrice = productCard.find_element(
-            By.CLASS_NAME, "priceCard").text
+        name = productCard.find_element(
+            By.CSS_SELECTOR, "[class*='product_info_title-noMarginBottom']").text
 
-        productData = {"name": name, "active_sale": True if oldPrice else False, "old_price": oldPrice if oldPrice else None,
+        try:
+            img_src = productCard.find_element(
+                By.TAG_NAME, "img").get_attribute("src")
+        except:
+            img_src = None
+
+        try:
+            oldPrice = productCard.find_element(
+                By.CSS_SELECTOR, "[class*='strikeThrough']").text
+        except:
+            oldPrice = None
+
+        try: 
+            currentPrice = productCard.find_element(
+                By.CSS_SELECTOR, "div[class*='price_vista']").text
+        except:
+            currentPrice = None
+
+        productData = {"loja": "Pichau", "name": name, "img": img_src, "active_sale": True if oldPrice else False, "old_price": oldPrice if oldPrice else None,
                        "price": currentPrice, "url": url, "date": datetime.today().strftime('%Y-%m-%d')}
 
         pageData.append(productData)
@@ -29,7 +46,8 @@ def extractData():
 
 
 def navigateToNextPage():
-    nextButton = driver.find_element(By.CLASS_NAME, "nextLink")
+    nextButton = driver.find_element(
+        By.CSS_SELECTOR, '[aria-label="Go to next page"]')
     driver.execute_script(
         "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", nextButton)
     time.sleep(0.5)
@@ -37,7 +55,7 @@ def navigateToNextPage():
 
 
 options = Options()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--window-size=1920,1080")
 driver = webdriver.Chrome(options=options)
 
@@ -46,10 +64,9 @@ driver.get("https://www.pichau.com.br/hardware")
 
 title = driver.title
 
-consentButton = driver.find_element(By.ID, "onetrust-accept-btn-handler")
-consentButton.click()
 data = []
 
+pagesCount = 0
 while True:
 
     pageData = extractData()
@@ -65,13 +82,18 @@ while True:
 
     WebDriverWait(driver, 5*60).until(EC.url_changes(driver.current_url))
     WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "productCard"))
+        EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "[class*='product_info_title-noMarginBottom']"))
     )
+
+    pagesCount += 1
+    if pagesCount >= 50:
+        break
 
 finish_time = datetime.now()
 timestamp = finish_time.strftime("%Y-%m-%d_%H-%M")
 
-with open(f"products_{timestamp}.json", "w", encoding="utf-8") as f:
+with open(f"products_pichau_{timestamp}.json", "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
 driver.quit()
